a6fe447b6cded3ce22507293729942cfca1ef232
Shane Eckenrode <sue2@psu.edu>
2015-03-23 02:20:39 -0400

    Reworked Health and Shield Bars, Health and Shield work properly now. Added logic to disable gun when recharging shield. Adjusted movement disabling when dead and recharging. Fixed minor bugs and glitches.

--- Assets/Scripts/spaceshipScript.js
+++ Assets/Scripts/spaceshipScript.js
@@ -5,37 +5,27 @@ public var bullet_count: int;
 public var levelFinished: boolean;
 public var playerDie: boolean;
 public var scoreObj: spawnScript;
-public var score: int;
-public var health: RectTransform;
-private var yPos: float;
-public var xMin: float;
-public var xMax: float;
-public var currentHP: float;
-public var maxHP: int;
 public var shieldAnim: ParticleSystem;
-var shoot: AudioClip;
-var explode: AudioClip;
-var clones;
+public var score: int;
+public var shoot: AudioClip;
+public var explode: AudioClip;
+public var regen: AudioClip;
+public var clones;
 private var animator: Animator;
+private var canShoot: boolean;
+private var canMove: boolean;
+
+public var health: UnityEngine.UI.Slider;
+public var shield: UnityEngine.UI.Slider;
 
 function Start () {
-	yPos = health.position.y;
-	xMax = health.position.x;
-	xMin = health.position.x - health.rect.width;
-	currentHP = maxHP;
-	
 	bullet_count = 0;
 	scoreObj = GetComponent("spawnScript");
 	levelFinished = false;
 	playerDie = false;
 	animator = GetComponent("Animator");
-}
-
-//Maps health bar to values based on the positions of the bar. See video for details
-//https://www.youtube.com/watch?v=NgftVg3idB4&t=1835
-function mapHPValues(x: float, inMin: float, inMax: float, outMin: float, outMax: float){
-	return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
-	
+	canShoot = true;
+	canMove = true;
 }
 
 function OnTriggerEnter2D(obj : Collider2D) {  
@@ -44,9 +34,17 @@ function OnTriggerEnter2D(obj : Collider2D) {
 
     // If player collided with enemy
     if (name == "enemy(Clone)") {
-    	setHP();
+    	if(shield.value > 0){
+    		shield.value -= .1;
+    	}else if(shield.value <= 0){
+	    	//Health Slider scale range is from 0 to 1
+	    	//Subtracting .1 is equivalent to subtracting 10% of the bar, or 10HP.
+	    	health.value -= .21;
+	    }
     	Destroy(obj.gameObject);
-	    if(currentHP <= 30){
+    	if(health.value <= 0){
+	    	Destroy(obj.gameObject);
+	    	
 	    	//Disables collider so objects pass through while death animation plays.
 	        gameObject.GetComponent.<Collider2D>().enabled = false;
 	        // Plays player death animation.
@@ -59,26 +57,29 @@ function OnTriggerEnter2D(obj : Collider2D) {
 	        Destroy(gameObject, 0.8);
 	        Destroy(obj.gameObject); // You lose!
 	        spawnScript.end_game_trigger();
-	        }
+	    	}
 	    }
     }
 }
 
 // This function gets called ~60 times per second
 function Update() {  
+	// movement depending on if player is alive or dead
+    if(playerDie){
+    	canMove = false;
+    }
+
     // GetAxis() returns a value between 1 and -1
     // Depending on which arrow key is pressed
-
-    // movement depending on if player is alive or dead
-    if(playerDie == true){
-    	GetComponent.<Rigidbody2D>().velocity.x = 0;
-    	GetComponent.<Rigidbody2D>().velocity.y = 0;
-    }else{
-    	GetComponent.<Rigidbody2D>().velocity.x = Input.GetAxis("Horizontal") * 10;
+    if(canMove){
+		GetComponent.<Rigidbody2D>().velocity.x = Input.GetAxis("Horizontal") * 10;
     	GetComponent.<Rigidbody2D>().velocity.y = Input.GetAxis("Vertical") * 10;
-    }
+	}else{
+		GetComponent.<Rigidbody2D>().velocity.x = 0;
+    	GetComponent.<Rigidbody2D>().velocity.y = 0;
+	}
     
-    if (Input.GetKey("z") && bullet_count < 1) {
+    if (Input.GetKey("z") && bullet_count < 1 && canShoot) {
     	// create a new bullet at "transform.position"
     	// Which is the current position of the ship
     	
@@ -91,12 +92,15 @@ function Update() {
     
     //If shift is held, particle system emits while restricting the player movement.
     if(Input.GetKey(KeyCode.LeftShift)){
+    	//Increases shield slightly every second.
+    	shield.value += .001;
     	shieldAnim.Emit(3);
-    	GetComponent.<Rigidbody2D>().velocity.x = 0;
-    	GetComponent.<Rigidbody2D>().velocity.y = 0;
+    	GetComponent.<AudioSource>().PlayOneShot(regen, 0.4);
+		canMove = false;
+		canShoot = false;
     }else{
-    	GetComponent.<Rigidbody2D>().velocity.x = Input.GetAxis("Horizontal") * 10;
-    	GetComponent.<Rigidbody2D>().velocity.y = Input.GetAxis("Vertical") * 10;
+    	canMove = true;
+    	canShoot = true;
     }
     
     //Grabs the score and checks if the player have beaten the level.
@@ -111,22 +115,4 @@ function Update() {
     if(levelFinished){
     	Application.LoadLevel("UpgradeScene");
     }
-}
-
-//Moves the position of the health bar to simulate losing HP.
-function updateHealth(){
-	var currentX: float = mapHPValues(currentHP, 0, maxHP, xMin, xMax);
-	
-	health.transform.position = new Vector3(currentX, yPos);
-}
-
-//Returns the current HP.
-function getHP(){
-	return currentHP;
-}
-
-//Sets your current HP to a lower number when you are hit and updates the heathbar position.
-function setHP(){
-	currentHP-= 10;
-	updateHealth();
 }
